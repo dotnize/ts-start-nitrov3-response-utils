19f9be3fb9b4837350a720242837dbaf45ea7559
Nathaniel Tampus <nate@mugnavo.com>
2025-09-25 11:40:41 +0800

    update test endpoint

--- src/routes/api/test.ts
+++ src/routes/api/test.ts
@@ -5,8 +5,9 @@ export const Route = createFileRoute("/api/test")({
   server: {
     handlers: {
       GET: async (req) => {
+        // this will not result in error, but just stuck in a blank page
         setResponseHeader("Location", "https://tanstack.com");
-        return new Response(null, { status: 302 });
+        return new Response(null);
       },
     },
   },
